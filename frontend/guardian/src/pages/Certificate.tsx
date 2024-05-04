6e4df39fab7cf877b2eaa3373679d32005d99189
elies <elie.stroun@epitech.eu>
2024-05-04 12:20:31 +0200

    feat(frontend): moving frontend/* in frontend/guardian

---
+++ frontend/guardian/src/pages/Certificate.tsx
@@ -0,0 +1,8 @@
+// src/pages/Certificate.tsx
+import React from 'react';
+
+const Certificate: React.FC = () => {
+    return <h1>Certificate Page</h1>;
+}
+
+export default Certificate;
---
+++ frontend/guardian/src/pages/Library.tsx
@@ -0,0 +1,8 @@
+// src/pages/Library.tsx
+import React from 'react';
+
+const Library: React.FC = () => {
+    return <h1>Library Page</h1>;
+}
+
+export default Library;
