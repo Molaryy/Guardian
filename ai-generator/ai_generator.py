de615cedb13e94dbb46c1817579f513a478bc9f7
matthiasvonrakowski <matthias.von-rakowski@epitech.eu>
2024-05-04 16:56:22 +0200

    feature(ai-generator) : Recovered and working for received and send to the client

--- ai-generator/ai_generator.py
+++ ai-generator/ai_generator.py
@@ -1,17 +1,14 @@
-import flask
 import openai
-from ai_flask_app import get_prompt, send_image_url
+from flask import Flask, requests, jsonify, request
+
 class LLM:
-    def __init__(self, api_key, model="dall-e-3", chat_history=[], configuration_prompt="you are chatGPt"):
-        openai.api_key = api_key
+    def __init__(self, model="dall-e-3", chat_history=[], configuration_prompt="you are chatGPt"):
+        openai.api_key = openai.OpenAI().api_key
         self.model = model
         self.chat_history = chat_history
         self.configuration_prompt = configuration_prompt
 
     def ask_gpt(self, user_input: str):
-
-        recent_messages = self.chat_history[-5:]
-
         response = openai.images.generate(
             model=self.model,
             prompt=user_input,
@@ -20,29 +17,22 @@ class LLM:
             n=1)
         return response.data[0].url
 
-    def chat(self, max_tokens=50, temperature=0.7):
-        print("You are now chatting with Dall-e, the only mighty one! Type 'exit' to end the conversation.")
-        while True:
-            user_input = get_prompt()
+llm = LLM()
+app = Flask(__name__)
 
-            if user_input.lower() == 'exit':
-                print("Goodbye!")
-                break
-
-            self.chat_history.append(user_input)
-            response = self.ask_gpt(user_input)
-            send_image_url(response)
-            self.chat_history.append(response)
-
-    def clear_chat_history(self):
-        self.chat_history = []
-
-def main():
-    OPENAI_API_KEY = "" ##TODO: Add your OpenAI API key here (ask tomorrow to the assistant)
-    app = app_creation()
-    llm = LLM(OPENAI_API_KEY)
-    llm.chat()
-
+@app.route('/openai/endpoint', endpoint='url', methods=['GET'])
+def get_prompt():
+    try :
+        json_file = request.get_json()
+        prompt = json_file['prompt']
+    except Exception as e:
+        return jsonify({"error": "404"})
+    msg = llm.ask_gpt(prompt)
+    if msg.isalnum():
+        return jsonify({"error": msg})
+    return jsonify({"url": msg})
+    # respond = requests.post("http://localhost:5000/openai/images/generate", json={"url": msg})
+    # return jsonify(respond.json())
 
 if __name__ == "__main__":
-    main()
+    app.run()
