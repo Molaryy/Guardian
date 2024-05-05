2ed687a3ec511f397f183822242b4d667be025c0
matthiasvonrakowski <matthias.von-rakowski@epitech.eu>
2024-05-05 11:54:01 +0200

    feature(recovered_ai_generator) : add image comparaison

--- backend/openai/ai_generator.py
+++ backend/openai/ai_generator.py
@@ -1,4 +1,8 @@
 import openai
+import requests
+import numpy as np
+import cv2
+from urllib.request import urlopen, Request
 from flask import Flask, jsonify, request, make_response
 
 class LLM():
@@ -33,6 +37,43 @@ def get_prompt():
         return _corsify_actual_response(jsonify({"error": e}))
     msg = llm.ask_gpt(prompt)
     return _corsify_actual_response(jsonify({"url": msg}))
+
+
+@app.route('/openai/image/compare', methods=['POST', 'OPTIONS'])
+def compare_images():
+    if request.method == 'OPTIONS':
+        return _build_cors_preflight_response()
+    try:
+        json_file = request.get_json()
+        app.logger.info(json_file)
+        url1 = json_file['image']
+        url2 = json_file['image_to_compare']
+
+        secu = Request(url1, headers={'User-Agent': 'Mozilla/5.0'})
+        req = urlopen(secu).read()
+        arr = np.asarray(bytearray(req), dtype=np.uint8)
+        image1 = cv2.imdecode(arr, -1)
+        secu = Request(url2, headers={'User-Agent': 'Mozilla/5.0'})
+        req = urlopen(secu).read()
+        arr = np.asarray(bytearray(req), dtype=np.uint8)
+        image2 = cv2.imdecode(arr, -1)
+    except Exception as e:
+        app.logger.error(e)
+        return _corsify_actual_response(jsonify({"error": e}))
+    mse_value = mse(image1, image2)
+    return _corsify_actual_response(jsonify({"mse_val": mse_value}))
+
+def mse(img1, img2):
+    if len(img1.shape) == 1:
+        return -1
+    h1, w1 = img1.shape[:2]
+    h2, w2 = img2.shape[:2]
+    if h1 != h2 or w1 != w2:
+        img1 = cv2.resize(img1, (w2, h2))
+    diff = cv2.subtract(img1, img2)
+    err = np.sum(diff ** 2)
+    mse = err / (float(h2 * w2))
+    return mse
 
 def _build_cors_preflight_response():
     response = make_response()
