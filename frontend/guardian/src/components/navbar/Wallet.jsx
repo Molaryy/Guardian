77c7af8fadc23babcf599cd82639b138357a4af0
mohammed <molaryyios@gmail.com>
2024-05-05 12:57:48 +0200

    fix(new nft): create nft qr code

--- frontend/guardian/src/components/navbar/Wallet.jsx
+++ frontend/guardian/src/components/navbar/Wallet.jsx
@@ -1,39 +1,47 @@
 import {Xumm} from 'xumm'
 import {useState} from "react";
 import './navbar.scss'
-
+import {convertStringToHex} from 'xrpl';
 
 const xumm = new Xumm(import.meta.env.VITE_XAMAN_API_KEY)
 
-const Wallet = () => {
+const Wallet = ({setUser}) => {
     const [account, setAccount] = useState('')
 
-    xumm.user.account.then(a => setAccount(a ?? ''))
+    xumm.user.account.then(a => {
+        setUser(a ?? '');
+        setAccount(a ?? '')
+    })
 
     console.log(account);
     const logout = () => {
         xumm.logout()
+        setUser('');
         setAccount('')
     }
+
+
 
     const shortenedAddress = account ?  account?.substring(0, 2) + "..." + account?.substring(account?.length - 5) : '';
 
     return (
-        <button className={"wallet-bar"}>
-            <p className={"ellipsis"}>{shortenedAddress}</p>
-            {
-                account === '' && !xumm.runtime.xapp
-                    ? <button className={"button-sign"} onClick={xumm.authorize}>Sign in</button>
-                    : ''
-            }
-            {
-                account !== ''
-                    ? <>
-                        <button onClick={logout}>Sign Out</button>
-                    </>
-                    : ''
-            }
-        </button>
+        <>
+            <button className={"wallet-bar"}>
+                <p className={"ellipsis"}>{shortenedAddress}</p>
+                {
+                    account === '' && !xumm.runtime.xapp
+                        ? <button className={"button-sign"} onClick={xumm.authorize}>Sign in</button>
+                        : ''
+                }
+                {
+                    account !== ''
+                        ? <>
+                            <button onClick={logout}>Sign Out</button>
+                        </>
+                        : ''
+                }
+            </button>
+        </>
     )
 }
 
